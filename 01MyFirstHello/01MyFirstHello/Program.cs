3995e9baa1a4063df8fd43360bc1447559794f9f
Roland Pachinger <pacr@htlvb.at>
2021-02-03 18:07:38 +0100

    Topic 1+2

---
+++ 01MyFirstHello/01MyFirstHello/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyFirstHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello HTL-World!");
+            Console.WriteLine("These are my known facts...");
+
+            // Ausgabe eines Zeilenumbruchs
+            Console.Write("\n");
+            // Console.WriteLine() = Console.Write("\n")
+
+            // Jetzt fragst du nach dem Namen
+            Console.Write("Please enter your name: ");
+            // Ich merke mir den eingegebenen Namen in der Text-Variable myName
+            string myName = Console.ReadLine();
+            // Console.WriteLine(myName);
+            Console.Write("Please enter your phonenumber:");
+            string phone = Console.ReadLine();
+            Console.WriteLine(" ####################################");
+            Console.WriteLine(" #\t" + myName);
+            Console.WriteLine(" #\tBahnhofstraße 42");
+            Console.WriteLine(" #\t4840 Vöcklabruck");
+            Console.WriteLine(" #\t" + phone);
+            Console.WriteLine(" #\tpacr@htlvb.at");
+            Console.WriteLine(" #\thttps://www.htlvb.at");
+            Console.WriteLine(" ####################################");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 02MyOldimeter/02MyOldimeter/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oldimeter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			Das soll euer Programm anzeigen:
+
+				*** Altersrechner (Stand 2020) ***
+	
+				Ich heiße Vorname Nachname.
+				Ich bin schon ... Jahre alt.
+				Nächstes Jahr bin ich ... Jahre alt.
+	
+				Mein Opa ist ... Jahre alt.
+				Nächstes Jahr ist er ... Jahre alt.
+	
+				In fünf Jahren (2025) ist Opa ... Jahre und ich ... Jahre alt!
+				Gemeinsam sind wir dann ... Jahre alt (falls Opa dann noch lebt).
+	
+	
+			Zahlen sollen mit Variablen dargestellt werden.
+			Eine Altersänderung soll nur an einer Stelle gemacht werden müssen
+			Das Jahr soll auch als Variable angegeben werden.
+			für Spezialisten: Auch die Namen sollen mit Variablen dargestellt werden
+			*/
+			Console.WriteLine("*** Altersrechner (Stand 2020) ***\n\n");
+
+			Console.Write("Gib deinen Vornamen ein: ");
+			string firstName = Console.ReadLine();
+
+			Console.Write("Gib deinen Nachnamen ein: ");
+			string lastName = Console.ReadLine();
+
+			Console.Write("Wie alt bist du? ");
+			// Alter wird als Text (string) zurück geliefert
+			string myAgeAsString = Console.ReadLine();
+			// String in Integer umwandeln
+			int myAge = Convert.ToInt32(myAgeAsString);
+
+			Console.Write("Wie alt ist Opa? ");
+			int opaAge;
+			// opaAge = Convert.ToInt32(Console.ReadLine());
+			int.TryParse(Console.ReadLine(), out opaAge);
+			// mit Int.Parse() oder Int.TryParse() testen
+
+			int year = 2020;
+
+			Console.WriteLine("Du heißt " + firstName + " " + lastName);
+			Console.WriteLine("Du bin schon " + myAge + " Jahre alt.");
+			Console.WriteLine("Nächstes Jahr bist du " + (myAge + 1) + " Jahre alt.\n\n");
+
+			Console.WriteLine("Dein Opa ist " + opaAge + " Jahre alt.");
+			Console.WriteLine("Nächstes Jahr ist er " + (opaAge + 1) + " Jahre alt.");
+
+			Console.WriteLine("In fünf Jahren (" + (year + 5) + ") ist Opa " + (opaAge + 5) + " Jahre und du " + (myAge + 5) + " Jahre alt!");
+			Console.WriteLine("Gemeinsam seid ihr dann " + (opaAge + 5 + myAge + 5) + " Jahre alt (falls Opa das erlebt).");
+			Console.ReadLine();
+		}
+
+	}
+}
